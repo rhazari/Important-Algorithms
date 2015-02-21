14759cf265a805616cef73223e4cdb23bd5a3a03
John Harvard <jharvard@appliance.localdomain>
2015-02-21 17:55:06 -0500

    Use of STL Library

---
+++ STL_Library/map_2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <algorithm>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+	const string delims(" \t,.;'");
+	map<string, int> words;
+	string line, str;
+
+	ifstream myfile("stairways_heaven.txt", ios_base::in);
+	while(getline(myfile,line))
+	{
+		string::size_type beg, end;
+		beg = line.find_first_not_of(delims);
+		while(beg != string::npos)
+		{
+			end = line.find_first_of(delims,beg);
+			if (end == string::npos)
+			{
+				end = line.length();	
+			}
+			str.assign(line,beg,end-beg+1);
+			++words[str];
+			beg = line.find_first_not_of(delims,end);
+		}
+	}
+
+	map<string, int>::const_iterator itr;
+	for(itr = words.begin(); itr != words.end(); itr++)
+	{
+		cout<<itr->first<<" : "<<itr->second<<"\n";
+	}
+
+	myfile.close();
+	return 0;
+}
