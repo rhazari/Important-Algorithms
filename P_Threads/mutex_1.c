2de233a68cbc83982e94784940511394658892f7
John Harvard <jharvard@appliance.localdomain>
2015-03-08 00:36:13 -0500

    add threads folder

---
+++ P_Threads/mutex_1.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#define N 5
+#define L 20
+
+typedef struct
+{
+	int *array;
+	int length;
+	int sum;
+}
+MyData;
+
+MyData mData;
+pthread_t myThread[N];
+pthread_mutex_t mutex;
+
+void *threadWork(void *arg)
+{
+	int offset = (int)arg;
+	int j, sum = 0;
+	int start, end;
+	start = offset*mData.length;
+	end = start + mData.length;
+
+	for( j = start; j < end; j++ )
+	{
+		sum += mData.array[j];
+	}
+	printf("Thread id: %d, Sum calculated: %d\n",offset, sum);
+	
+	pthread_mutex_lock(&mutex);
+	mData.sum += sum;
+	pthread_mutex_unlock(&mutex);
+	
+	pthread_exit((void*) 0);
+}
+
+int main()
+{
+	void *status;
+	
+	int k;
+	int *a = (int*)malloc(sizeof(int)*L*N);
+	for( k = 0; k < N*L; k++ )
+	{
+		a[k] = k+1; 
+	}
+	
+	mData.length = L;
+	mData.array = a;
+	mData.sum = 0;
+
+	pthread_mutex_init(&mutex, NULL);
+	
+	for( k = 0; k < N; k++	)
+	{
+		pthread_create(&myThread[k], NULL, threadWork, (void*)k );
+	}
+
+	for( k = 0; k < N; k++ )
+	{
+		pthread_join(myThread[k], &status);
+	}
+
+	printf("Total sum from threads = %d\n", mData.sum);
+	free(a);
+	
+	pthread_mutex_destroy(&mutex);
+	pthread_exit(NULL);
+	
+	return 0;
+}
---
+++ P_Threads/semaphore_1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+sem_t s;
+
+void *task(void* arg)
+{
+	printf("%s work completed\n",(char*)arg);
+	sem_post(&s);
+	
+	//return NULL;
+}
+
+int main()
+{
+	sem_init(&s, 0, 0);
+	
+	printf("Main thread starts\n");
+	pthread_t thread;
+	pthread_create(&thread, 0, task, (void*)"Child thread");
+	
+	sem_wait(&s);
+	printf("Main thread ends here\n");
+	return 0;
+}
