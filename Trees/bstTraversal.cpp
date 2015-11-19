3720e0196059587170990c4b80ed0de82cb2c424
rhazari <rhazari@g.clemson.edu>
2015-11-18 23:07:58 -0800

    pre order traversal of BST

---
+++ Trees/bstTraversal.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <cstdlib>
+#include <stack>
+
+using namespace std;
+
+typedef struct Tree {
+	int data;
+	struct Tree *left;
+	struct Tree *right;
+} 
+Tree;
+
+void insert(struct Tree **root, int value) {
+	if ( NULL == (*root) ){
+		Tree *temp = new Tree;
+		temp->data = value;
+		temp->left = NULL;
+		temp->right = NULL;
+		(*root) = temp;
+	}
+	else {
+		if( (*root)->data > value ) {
+			insert(&((*root)->left), value);
+		}
+		else if ( (*root)->data < value ) {
+			insert(&((*root)->right), value);
+		}
+	}
+}
+
+void iterativePreorder(struct Tree* root){
+	stack<Tree*> treeStack;
+	treeStack.push(root);
+
+	Tree *temp;
+	while( !treeStack.empty() ){
+		temp = treeStack.top();
+		treeStack.pop();
+		cout<<temp->data<<"\t";
+
+		if( NULL != temp->right ){
+			treeStack.push(temp->right);
+		}
+		if( NULL != temp->left ){
+			treeStack.push(temp->left);
+		}
+	}
+}
+
+int main() {
+	Tree *root = NULL;
+	insert(&root, 17);
+	insert(&root, 9);
+	insert(&root, 23);
+	insert(&root, 11);
+	insert(&root, 21);
+	insert(&root, 7);
+	insert(&root, 31);
+	insert(&root, 13);
+
+	cout<<"Iterative Pre Order Traversal of BST"<<endl;
+	iterativePreorder(root);
+	cout<<endl;
+
+	return 0;
+}
