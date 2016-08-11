2a11d0b9ebc92daf2d56b67d71b895644f39a569
rhazari <rhazari@g.clemson.edu>
2016-08-10 18:47:55 -0700

    new code files

---
+++ Strings&Arrays/subsets.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+	string str("raihan");
+	int sz = str.size();
+	int j, k, num;
+	static int count;
+	num = pow(2,sz);
+	for(k = 1; k < num; ++k){
+		cout<<++count<<"->";
+		for(j = 0; j < sz; ++j){
+			if( k & (1<<j)){
+				cout<<str[j];
+			}
+		}
+		cout<<endl;
+	}
+	//cout<<num<<endl;
+	return 0;
+}
