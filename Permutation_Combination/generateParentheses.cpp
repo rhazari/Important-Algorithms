7b9058495b9d1e23bcea3281c0e360805c1c1697
rhazari <rhazari@g.clemson.edu>
2017-09-05 21:19:20 -0700

    combination 101

---
+++ Permutation_Combination/generateParentheses.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+void helper(vector<string>& vec, int left, int right, string str){
+	if(left > right)
+		return;
+	if(left == 0 && right == 0){
+		vec.push_back(str);
+		return;
+	}
+	if(left > 0)
+		helper(vec, left-1, right, str+"(");
+	if(right > 0)
+		helper(vec, left, right-1, str+")");
+}
+
+int main(){
+	vector<string> vec;
+	helper(vec, 3, 3, "");
+
+	cout<<"Valid Parentheses..."<<endl;
+	for(const auto& s: vec)
+		cout<<s<<"\n";
+	cout<<endl;
+	return 0;
+}
